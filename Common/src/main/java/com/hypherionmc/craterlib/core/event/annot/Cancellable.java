f4d9e45427d9a5468c9bc8a0e0fca7543919dad4
hypherionmc <hypherionmc@gmail.com>
2024-05-09 14:28:27 +0200

    [DEV] Convert to new porting system

--- Common/src/main/java/com/hypherionmc/craterlib/core/event/annot/Cancellable.java
+++
@@ -1,8 +0,0 @@
-package com.hypherionmc.craterlib.core.event.annot;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Cancellable {
-}
