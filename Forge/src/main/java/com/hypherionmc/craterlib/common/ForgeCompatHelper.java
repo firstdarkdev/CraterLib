261ea4c3f8d99b7d31bab842813cf865b272595f
hypherionmc <hypherionmc@gmail.com>
2024-05-09 19:19:15 +0200

    Fuck sakes

--- Forge/src/main/java/com/hypherionmc/craterlib/common/ForgeCompatHelper.java
+++ Forge/src/main/java/com/hypherionmc/craterlib/common/ForgeCompatHelper.java
@@ -1,18 +1,13 @@
 package com.hypherionmc.craterlib.common;
 
 import com.hypherionmc.craterlib.core.platform.CompatUtils;
-import com.hypherionmc.craterlib.core.platform.ModloaderEnvironment;
 import com.hypherionmc.craterlib.nojang.world.entity.player.BridgedPlayer;
-import redstonedubstep.mods.vanishmod.VanishUtil;
 
 public class ForgeCompatHelper implements CompatUtils {
 
     @Override
     public boolean isPlayerActive(BridgedPlayer player) {
-        if (!ModloaderEnvironment.INSTANCE.isModLoaded("vmod"))
-            return true;
-
-        return VanishUtil.isVanished(player.toMojangServerPlayer());
+        return true;
     }
 
     @Override
