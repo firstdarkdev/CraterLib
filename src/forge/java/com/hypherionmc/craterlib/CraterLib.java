c9f782f321d5d89861e75a1ee5ef3cddeed41873
Colin James Wood <christhefennec@gmail.com>
2025-11-15 14:32:34 +0000

    workaround client setup changes since DistExecutor is deprecated

--- src/forge/java/com/hypherionmc/craterlib/CraterLib.java
+++ src/forge/java/com/hypherionmc/craterlib/CraterLib.java
@@ -4,22 +4,23 @@ import com.hypherionmc.craterlib.api.events.client.LateInitEvent;
 import com.hypherionmc.craterlib.common.ForgeServerEvents;
 import com.hypherionmc.craterlib.core.event.CraterEventBus;
 import com.hypherionmc.craterlib.core.networking.CraterPacketNetwork;
+import com.hypherionmc.craterlib.core.networking.PacketRegistry;
 import com.hypherionmc.craterlib.core.networking.data.PacketSide;
 import com.hypherionmc.craterlib.network.CraterForgeNetworkHandler;
 import com.hypherionmc.craterlib.nojang.client.BridgedMinecraft;
 import net.minecraft.client.Minecraft;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.bus.BusGroup;
-import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLanguageProvider;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLLoader;
+import com.hypherionmc.craterlib.nojang.client.BridgedOptions;
 
 @Mod(CraterConstants.MOD_ID)
 public class CraterLib {
+    private final PacketRegistry handler;
 
     public CraterLib(FMLJavaModLoadingContext context) {
         BusGroup modBusGroup = context.getModBusGroup();
@@ -28,13 +29,17 @@ public class CraterLib {
 
         //https://www.reddit.com/r/MinecraftForge/comments/1mqinfr/help_with_mod_coding/
         FMLCommonSetupEvent.getBus(modBusGroup).addListener(this::commonSetup);
+        FMLClientSetupEvent.getBus(modBusGroup).addListener(this::clientSetup);
+
+        handler = new CraterForgeNetworkHandler(FMLLoader.getDist().isClient() ? PacketSide.CLIENT : PacketSide.SERVER);
     }
 
     public void commonSetup(FMLCommonSetupEvent evt) {
-        new CraterPacketNetwork(new CraterForgeNetworkHandler(FMLLoader.getDist().isClient() ? PacketSide.CLIENT : PacketSide.SERVER));
-        DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> {
-            LateInitEvent event = new LateInitEvent(new BridgedMinecraft(), BridgedOptions.of(Minecraft.getInstance().options));
-            CraterEventBus.INSTANCE.postEvent(event);
-        });
+        new CraterPacketNetwork(handler);
+    }
+
+    public void clientSetup(FMLClientSetupEvent evt) {
+        LateInitEvent event = new LateInitEvent(new BridgedMinecraft(), BridgedOptions.of(Minecraft.getInstance().options));
+        CraterEventBus.INSTANCE.postEvent(event);
     }
 }
